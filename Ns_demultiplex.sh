5c49a735899923a4abd3b6962d726c3c165f4f9b
jms53460 <jms53460@uga.edu>
2024-04-05 16:30:14 -0400

    Updated output and error locations, added cd

--- Ns_demultiplex.sh
+++ Ns_demultiplex.sh
@@ -5,11 +5,12 @@
 #SBATCH --cpus-per-task=6                               # Number of cores per task
 #SBATCH --mem=50gb                                      # Total memory for job
 #SBATCH --time=12:00:00                                 # Time limit hrs:min:sec
-#SBATCH --output=/work/gene8940/jms53460/Ns_dm.out      # Location of standard output file
-#SBATCH --error=/work/gene8940/jms53460/Ns_dm.err       # Location of error log file
+#SBATCH --output=/home/jms53460/Ns_dm.out      # Location of standard output file
+#SBATCH --error=/home/jms53460/Ns_dm.err       # Location of error log file
 #SBATCH --mail-user=jms53460@uga.edu                    # Where to send mail
 #SBATCH --mail-type=END,FAIL                            # Mail events (BEGIN, END, FAIL, ALL)
 
+cd /home/jms53460
 source activate /home/jms53460/Fastq-Multx/
 
 for file in Raw_Data/*_R1_*.gz; do
