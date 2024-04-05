20215e843187f1ab06269afda5325b138b9bafc5
jms53460 <jms53460@uga.edu>
2024-04-05 16:26:12 -0400

    Made sh file to demultiplex tobacco raw data

---
+++ Ns_demultiplex.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --job-name=Ns_demultiplex                       # Job name
+#SBATCH --partition=batch                               # Partition (queue) name
+#SBATCH --ntasks=1                                      # Single task job
+#SBATCH --cpus-per-task=6                               # Number of cores per task
+#SBATCH --mem=50gb                                      # Total memory for job
+#SBATCH --time=12:00:00                                 # Time limit hrs:min:sec
+#SBATCH --output=/work/gene8940/jms53460/Ns_dm.out      # Location of standard output file
+#SBATCH --error=/work/gene8940/jms53460/Ns_dm.err       # Location of error log file
+#SBATCH --mail-user=jms53460@uga.edu                    # Where to send mail
+#SBATCH --mail-type=END,FAIL                            # Mail events (BEGIN, END, FAIL, ALL)
+
+source activate /home/jms53460/Fastq-Multx/
+
+for file in Raw_Data/*_R1_*.gz; do
+    filename=$(basename "$file")
+    file2=$(echo "$filename" | sed 's/_R1.*//' | sed 's/_R2_001.fastq.gz//')
+
+    if [ ! -f "Mapped_Data/demultiplexed/${file2}_dT-1s.fastq.gz" ]; then
+        module load fastp/0.23.2-GCC-11.3.0
+        fastp -w 6 -i "$file" -I "Raw_Data/${file2}_R2_001.fastq.gz" -o "Mapped_Data/demultiplexed/umi_${file2}_R1.fastq.gz" -O "Mapped_Data/demultiplexed/umi_${file2}_R2.fastq.gz" -A -Q -L --umi --umi_loc read1 --umi_len 10 --umi_prefix UMI
+
+        fastq-multx -b -B "CELSeq_barcodes.txt" -m 0 "Mapped_Data/demultiplexed/umi_${file2}_R2.fastq.gz" "Mapped_Data/demultiplexed/umi_${file2}_R1.fastq.gz" -o "Mapped_Data/demultiplexed/${file2}_R1.fastq.gz" "Mapped_Data/demultiplexed/${file2}_%.fastq.gz"  # Split read 2 file by CELseq barcodes. Require perfect match to barcode in expected location
+
+        find "Mapped_Data/demultiplexed/" -name "umi_*" -delete
+        find "Mapped_Data/demultiplexed/" -name "*s_R2*" -delete
+    fi
+done
+conda deactivate
