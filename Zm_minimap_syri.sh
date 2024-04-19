47957b78c9214a250adaab3466ec5b389e9b706e
jms53460 <jms53460@uga.edu>
2024-04-19 16:33:23 -0400

    Wrote code to map A188 assembly to B73 reference and use SyRI to make vcf

---
+++ Zm_minimap_syri.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=Zm_minimap_syri                                         # Job name
+#SBATCH --partition=batch                                                  # Partition (queue) name
+#SBATCH --ntasks=1                                                         # Single task job
+#SBATCH --cpus-per-task=6                                                  # Number of cores per task
+#SBATCH --mem=50gb                                                         # Total memory for job
+#SBATCH --time=12:00:00                                                    # Time limit hrs:min:sec
+#SBATCH --output=/scratch/jms53460/Maize_SGT_2022/Zm_minimap_syri.out      # Location of standard output file
+#SBATCH --error=/scratch/jms53460/Maize_SGT_2022/Zm_minimap_syri.err       # Location of error log file
+#SBATCH --mail-user=jms53460@uga.edu                                       # Where to send mail
+#SBATCH --mail-type=END,FAIL                                               # Mail events (BEGIN, END, FAIL, ALL)
+
+cd /scratch/jms53460/Maize_SGT_2022
+
+ml minimap2/2.26-GCCcore-12.2.0
+minimap2 -ax asm5 --eqx B73_v5_genome.fa A188_genome.fa > A188_aligned_to_B73.sam
+
+ml SyRI/1.6.3
+syri -c A188_aligned_to_B73.sam -r B73_v5_genome.fa -q A188_genome.fa -k -F S
