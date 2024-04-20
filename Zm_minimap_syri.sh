68ecae1d0cefb8524e148826c1cc3c16648d5749
jms53460 <jms53460@uga.edu>
2024-04-19 20:29:12 -0400

    Added more memory since minimap2 failed again due to running out of memory, also added more time and threaded minimap2

--- Zm_minimap_syri.sh
+++ Zm_minimap_syri.sh
@@ -3,8 +3,8 @@
 #SBATCH --partition=batch                                                  # Partition (queue) name
 #SBATCH --ntasks=1                                                         # Single task job
 #SBATCH --cpus-per-task=6                                                  # Number of cores per task
-#SBATCH --mem=150gb                                                         # Total memory for job
-#SBATCH --time=12:00:00                                                    # Time limit hrs:min:sec
+#SBATCH --mem=400gb                                                        # Total memory for job
+#SBATCH --time=24:00:00                                                    # Time limit hrs:min:sec
 #SBATCH --output=/scratch/jms53460/Maize_SGT_2022/Zm_minimap_syri.out      # Location of standard output file
 #SBATCH --error=/scratch/jms53460/Maize_SGT_2022/Zm_minimap_syri.err       # Location of error log file
 #SBATCH --mail-user=jms53460@uga.edu                                       # Where to send mail
@@ -13,7 +13,7 @@
 cd /scratch/jms53460/Maize_SGT_2022
 
 ml minimap2/2.26-GCCcore-12.2.0
-minimap2 -ax asm5 --eqx B73_v5_genome.fa A188_genome.fa > A188_aligned_to_B73.sam
+minimap2 -t 6 -ax asm5 --eqx B73_v5_genome.fa A188_genome.fa > A188_aligned_to_B73.sam
 
 ml SyRI/1.6.3
 syri -c A188_aligned_to_B73.sam -r B73_v5_genome.fa -q A188_genome.fa -k -F S
