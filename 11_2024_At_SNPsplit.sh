1eea7458577910b2b4eba2b0e35ffeb9eda1f70d
jms53460 <jms53460@uga.edu>
2024-12-19 18:28:28 -0500

    Updated SNPsplit

--- 11_2024_At_SNPsplit.sh
+++ 11_2024_At_SNPsplit.sh
@@ -4,13 +4,14 @@
 #SBATCH --ntasks=1                                                                  # Single task job
 #SBATCH --cpus-per-task=6                                                           # Number of cores per task
 #SBATCH --mem=50gb                                                                  # Total memory for job
-#SBATCH --time=12:00:00                                                             # Time limit hrs:min:sec
+#SBATCH --time=6:00:00                                                             # Time limit hrs:min:sec
 #SBATCH --output=/scratch/jms53460/11_2024_At/At_SNPsplit.out                       # Location of standard output file
 #SBATCH --error=/scratch/jms53460/11_2024_At/At_SNPsplit.err                        # Location of error log file
 #SBATCH --mail-user=jms53460@uga.edu                                                # Where to send mail
 #SBATCH --mail-type=END,FAIL                                                        # Mail events (BEGIN, END, FAIL, ALL)
 
 cd /scratch/jms53460/11_2024_At/
+mkdir At_SNPsplit
 ml SAMtools/1.16.1-GCC-11.3.0
 ml SNPsplit/0.6.0-GCC-11.3.0-Perl-5.34.1
 for file in "hisat2_out/"*_s.bam
@@ -33,4 +34,3 @@ do
     file2="${file:12:-14}"
     samtools sort -@ 6 "$file" -o At_SNPsplit/"$file2"_SNPsplit_g2.bam
 done
-
