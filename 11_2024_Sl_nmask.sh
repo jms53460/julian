54b22f641133b173ebea38fb1f241bad23a8cd17
jms53460 <jms53460@uga.edu>
2025-01-06 16:51:05 -0500

    N-masking genome

---
+++ 11_2024_Sl_nmask.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=Sl_nmask                                               # Job name
+#SBATCH --partition=batch                                                 # Partition (queue) name
+#SBATCH --ntasks=1                                                        # Single task job
+#SBATCH --cpus-per-task=1                                                 # Number of cores per task
+#SBATCH --mem=50gb                                                        # Total memory for job
+#SBATCH --time=12:00:00                                                    # Time limit hrs:min:sec
+#SBATCH --output=/scratch/jms53460/11_2024_Sl/Sl_nmask.out                # Location of standard output file
+#SBATCH --error=/scratch/jms53460/11_2024_Sl/Sl_nmask.err                 # Location of error log file
+#SBATCH --mail-user=jms53460@uga.edu                                      # Where to send mail
+#SBATCH --mail-type=END,FAIL                                              # Mail events (BEGIN, END, FAIL, ALL)
+
+cd /scratch/jms53460/11_2024_Sl
+ml BEDTools/2.30.0-GCC-12.2.0
+bedtools maskfasta -fi Sl_genome_12.fa -fo Sl_N-masked_genome.fa -bed syri.vcf -fullHeader
