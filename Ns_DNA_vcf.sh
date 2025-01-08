676a77a04f81902e461708adb1b9a3cd4e9c4c79
jms53460 <jms53460@uga.edu>
2025-01-08 11:30:50 -0500

    fixed typo

--- Ns_DNA_vcf.sh
+++ Ns_DNA_vcf.sh
@@ -12,12 +12,12 @@
 
 cd /scratch/jms53460/Tobacco_DNAseq
 module load SAMtools/1.16.1-GCC-11.3.0
-samtools index -@ 6 TW136_s.bam
-samtools index -@ 6 TW137_s.bam
+samtools index -@ 6 hisat2_out/TW136_s.bam
+samtools index -@ 6 hisat2_out/TW137_s.bam
 
 module load BCFtools/1.15.1-GCC-11.3.0
-bcftools mpileup -Ou --threads 6 -d 1000000 --min-MQ 60 -f Ns_genome.fna TW136_s.bam | bcftools call -Ou -m -v --threads 6 | bcftools filter -Oz -e 'QUAL<40 || DP<10' > TW136.vcf.gz
+bcftools mpileup -Ou --threads 6 -d 1000000 --min-MQ 60 -f Ns_genome.fna hisat2_out/TW136_s.bam | bcftools call -Ou -m -v --threads 6 | bcftools filter -Oz -e 'QUAL<40 || DP<10' > TW136.vcf.gz
 bcftools index TW136.vcf.gz
 
-bcftools mpileup -Ou --threads 6 -d 1000000 --min-MQ 60 -f Ns_genome.fna TW137_s.bam | bcftools call -Ou -m -v --threads 6 | bcftools filter -Oz -e 'QUAL<40 || DP<10' > TW137.vcf.gz
+bcftools mpileup -Ou --threads 6 -d 1000000 --min-MQ 60 -f Ns_genome.fna hisat2_out/TW137_s.bam | bcftools call -Ou -m -v --threads 6 | bcftools filter -Oz -e 'QUAL<40 || DP<10' > TW137.vcf.gz
 bcftools index TW137.vcf.gz
