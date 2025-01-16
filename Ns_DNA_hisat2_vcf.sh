7c1092e9538edd842b3f45264d51a1753525edcb
jms53460 <jms53460@uga.edu>
2025-01-16 12:30:50 -0500

    Remaking vcf with new consensus sequence

--- Ns_DNA_hisat2_vcf.sh
+++ Ns_DNA_hisat2_vcf.sh
@@ -11,20 +11,20 @@
 #SBATCH --mail-type=END,FAIL                                          # Mail events (BEGIN, END, FAIL, ALL)
 
 cd /scratch/jms53460/Tobacco_DNAseq
-mkdir hisat2_out2
-cp hisat2_out/*fastq.gz hisat2_out2
+mkdir hisat2_out4
+cp hisat2_out/*fastq.gz hisat2_out4
 
     ml HISAT2/3n-20201216-gompi-2022a
-    hisat2-build TW136_consensus.fa TW136_consensus_index
+    hisat2-build TW136_consensus2.fa TW136_consensus2_index
 	module load SAMtools/1.16.1-GCC-11.3.0
-	hisat2 -p 6 --dta -x TW136_consensus_index -1 "hisat2_out2/TW136_R1_001.fastq.gz" -2 "hisat2_out2/TW136_R2_001.fastq.gz" | samtools view -bS -> "hisat2_out2/TW136_unsorted.bam"
-	samtools sort -@ 6 "hisat2_out2/TW136_unsorted.bam" -o "hisat2_out2/TW136_s.bam"
-	hisat2 -p 6 --dta -x TW136_consensus_index -1 "hisat2_out2/TW137_R1_001.fastq.gz" -2 "hisat2_out2/TW137_R2_001.fastq.gz" | samtools view -bS -> "hisat2_out2/TW137_unsorted.bam"
-	samtools sort -@ 6 "hisat2_out2/TW137_unsorted.bam" -o "hisat2_out2/TW137_s.bam"
+	hisat2 -p 6 --dta -x TW136_consensus2_index -1 "hisat2_out4/TW136_R1_001.fastq.gz" -2 "hisat2_out4/TW136_R2_001.fastq.gz" | samtools view -bS -> "hisat2_out4/TW136_unsorted.bam"
+	samtools sort -@ 6 "hisat2_out4/TW136_unsorted.bam" -o "hisat2_out4/TW136_s.bam"
+	hisat2 -p 6 --dta -x TW136_consensus2_index -1 "hisat2_out4/TW137_R1_001.fastq.gz" -2 "hisat2_out4/TW137_R2_001.fastq.gz" | samtools view -bS -> "hisat2_out4/TW137_unsorted.bam"
+	samtools sort -@ 6 "hisat2_out4/TW137_unsorted.bam" -o "hisat2_out4/TW137_s.bam"
 
 
 module load BCFtools/1.15.1-GCC-11.3.0
-bcftools mpileup -Ou --threads 6 -d 1000000 --min-MQ 60 -f TW136_consensus.fa hisat2_out2/TW137_s.bam | bcftools call -Ou -m -v --threads 6 | bcftools filter -Oz -e 'QUAL<40 || DP<10' > TW137_consensus.vcf.gz
-bcftools index TW137_consensus.vcf.gz
-bcftools mpileup -Ou --threads 6 -d 1000000 --min-MQ 60 -f TW136_consensus.fa hisat2_out2/TW136_s.bam | bcftools call -Ou -m -v --threads 6 | bcftools filter -Oz -e 'QUAL<40 || DP<10' > TW136_consensus.vcf.gz
-bcftools index TW136_consensus.vcf.gz
+bcftools mpileup -Ou --threads 6 -d 1000000 --min-MQ 60 -f TW136_consensus2.fa hisat2_out4/TW136_s.bam | bcftools call -Ou -m -v --threads 6 | bcftools filter -Oz -e 'QUAL<40 || DP<10' > TW136_consensus2.vcf.gz
+bcftools index TW136_consensus2.vcf.gz
+bcftools mpileup -Ou --threads 6 -d 1000000 --min-MQ 60 -f TW136_consensus2.fa hisat2_out4/TW137_s.bam | bcftools call -Ou -m -v --threads 6 | bcftools filter -Oz -e 'QUAL<40 || DP<10' > TW137_consensus2.vcf.gz
+bcftools index TW137_consensus2.vcf.gz
