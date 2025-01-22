48ffe4b51d45a8c1349e1d5dca470bd82215c4d3
jms53460 <jms53460@uga.edu>
2025-01-21 21:00:52 -0500

    Prepared to rerun SNPsplit and featurecounts

--- 11_2024_Sl_SNPsplit.sh
+++ 11_2024_Sl_SNPsplit.sh
@@ -11,26 +11,26 @@
 #SBATCH --mail-type=END,FAIL                                                        # Mail events (BEGIN, END, FAIL, ALL)
 
 cd /scratch/jms53460/11_2024_Sl
-mkdir SNPsplit
+mkdir SNPsplit2
 ml SAMtools/1.16.1-GCC-11.3.0
 ml SNPsplit/0.6.0-GCC-11.3.0-Perl-5.34.1
-for file in "hisat2_out/"*_s.bam
+for file in "hisat2_out2/"*_s.bam
 do
-    file2="${file:11:-6}"
+    file2="${file:12:-6}"
 
-    SNPsplit --conflicting -o SNPsplit --snp_file Sl_SNPs.tab "$file"
-    samtools sort -@ 6 SNPsplit/"$file2"_s.allele_flagged.bam -o SNPsplit/"$file2"_SNPsplit.bam
+    SNPsplit --conflicting -o SNPsplit2 --snp_file Sl_SNPs.tab "$file"
+    samtools sort -@ 6 SNPsplit2/"$file2"_s.allele_flagged.bam -o SNPsplit2/"$file2"_SNPsplit.bam
     
 done
 
-for file in "SNPsplit/"*_s.genome1.bam
+for file in "SNPsplit2/"*_s.genome1.bam
 do
-    file2="${file:9:-14}"
-    samtools sort -@ 6 "$file" -o SNPsplit/"$file2"_SNPsplit_g1.bam
+    file2="${file:10:-14}"
+    samtools sort -@ 6 "$file" -o SNPsplit2/"$file2"_SNPsplit_g1.bam
 done
 
-for file in "SNPsplit/"*_s.genome2.bam
+for file in "SNPsplit2/"*_s.genome2.bam
 do
-    file2="${file:9:-14}"
-    samtools sort -@ 6 "$file" -o SNPsplit/"$file2"_SNPsplit_g2.bam
+    file2="${file:10:-14}"
+    samtools sort -@ 6 "$file" -o SNPsplit2/"$file2"_SNPsplit_g2.bam
 done
