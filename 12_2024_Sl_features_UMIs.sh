7608dd826cabd0d6ff912ad4f9b927a23249f96e
jms53460 <jms53460@uga.edu>
2025-01-21 21:25:04 -0500

    Prepared to rerun code for the rest of the tomato data

--- 12_2024_Sl_features_UMIs.sh
+++ 12_2024_Sl_features_UMIs.sh
@@ -3,67 +3,67 @@
 #SBATCH --partition=batch                                                     # Partition (queue) name
 #SBATCH --ntasks=1                                                            # Single task job
 #SBATCH --cpus-per-task=6                                                     # Number of cores per task
-#SBATCH --mem=50gb                                                            # Total memory for job
-#SBATCH --time=6:00:00                                                       # Time limit hrs:min:sec
+#SBATCH --mem=70gb                                                            # Total memory for job
+#SBATCH --time=12:00:00                                                       # Time limit hrs:min:sec
 #SBATCH --output=/scratch/jms53460/12_2024_Sl/Features_UMIs.out            # Location of standard output file
 #SBATCH --error=/scratch/jms53460/12_2024_Sl/Features_UMIs.err             # Location of error log file
 #SBATCH --mail-user=jms53460@uga.edu                                          # Where to send mail
 #SBATCH --mail-type=END,FAIL                                                  # Mail events (BEGIN, END, FAIL, ALL)
 
 cd /scratch/jms53460/12_2024_Sl
-mkdir featurecounts
-mkdir bams
-mkdir UMIcounts
-mkdir UMIcounts_g1
-mkdir UMIcounts_g2
+mkdir featurecounts2
+mkdir bams2
+mkdir UMIcounts2
+mkdir UMIcounts2_g1
+mkdir UMIcounts2_g2
 ml purge_dups/1.2.5-foss-2021b
 ml Miniconda3/23.5.2-0
 source activate /home/jms53460/subread-env
 
-featureCounts -T 6 -s 1 -a Sl_12.gff -t 'gene' -g 'ID' -o featurecounts/read_counts.tab --readExtension5 500 -R BAM SNPsplit/*_SNPsplit.bam
-featureCounts -T 6 -s 1 -a Sl_12.gff -t 'gene' -g 'ID' -o featurecounts/read_counts_g1.tab --readExtension5 500 -R BAM SNPsplit/*_SNPsplit_g1.bam
-featureCounts -T 6 -s 1 -a Sl_12.gff -t 'gene' -g 'ID' -o featurecounts/read_counts_g2.tab --readExtension5 500 -R BAM SNPsplit/*_SNPsplit_g2.bam
+featureCounts -T 6 -s 1 -a Sl_14.gff -t 'gene' -g 'ID' -o featurecounts2/read_counts.tab --readExtension5 500 -R BAM SNPsplit2/*_SNPsplit.bam
+featureCounts -T 6 -s 1 -a Sl_14.gff -t 'gene' -g 'ID' -o featurecounts2/read_counts_g1.tab --readExtension5 500 -R BAM SNPsplit2/*_SNPsplit_g1.bam
+featureCounts -T 6 -s 1 -a Sl_14.gff -t 'gene' -g 'ID' -o featurecounts2/read_counts_g2.tab --readExtension5 500 -R BAM SNPsplit2/*_SNPsplit_g2.bam
 
 conda deactivate
 
-for file in "featurecounts/"*SNPsplit.bam*
+for file in "featurecounts2/"*SNPsplit.bam*
 do
-    file2="${file:14:-22}"
-    if [ ! -f "UMIcounts/${file2}.tsv" ]; then
+    file2="${file:15:-22}"
+    if [ ! -f "UMIcounts2/${file2}.tsv" ]; then
 
         module load SAMtools/1.16.1-GCC-11.3.0
-        samtools sort -@ 6 "$file" -o "bams/$file2"
-        samtools index "bams/$file2"
+        samtools sort -@ 6 "$file" -o "bams2/$file2"
+        samtools index "bams2/$file2"
 
         module load UMI-tools/1.1.2-foss-2022a-Python-3.10.4
-        umi_tools count --per-gene --gene-tag=XT --assigned-status-tag=XS -I "bams/$file2" -S "UMIcounts/${file2}.tsv"
+        umi_tools count --per-gene --gene-tag=XT --assigned-status-tag=XS -I "bams2/$file2" -S "UMIcounts2/${file2}.tsv"
     fi
 done
 
-for file in "featurecounts/"*g1.bam*
+for file in "featurecounts2/"*g1.bam*
 do
-    file2="${file:14:-22}"
-    if [ ! -f "UMIcounts_g1/${file2}.tsv" ]; then
+    file2="${file:15:-22}"
+    if [ ! -f "UMIcounts2_g1/${file2}.tsv" ]; then
 
         module load SAMtools/1.16.1-GCC-11.3.0
-        samtools sort -@ 6 "$file" -o "bams/$file2"
-        samtools index "bams/$file2"
+        samtools sort -@ 6 "$file" -o "bams2/$file2"
+        samtools index "bams2/$file2"
 
         module load UMI-tools/1.1.2-foss-2022a-Python-3.10.4
-        umi_tools count --per-gene --gene-tag=XT --assigned-status-tag=XS -I "bams/$file2" -S "UMIcounts_g1/${file2}.tsv"
+        umi_tools count --per-gene --gene-tag=XT --assigned-status-tag=XS -I "bams2/$file2" -S "UMIcounts2_g1/${file2}.tsv"
     fi
 done
 
-for file in "featurecounts/"*g2.bam*
+for file in "featurecounts2/"*g2.bam*
 do
-    file2="${file:14:-22}"
-    if [ ! -f "UMIcounts_g2/${file2}.tsv" ]; then
+    file2="${file:15:-22}"
+    if [ ! -f "UMIcounts2_g2/${file2}.tsv" ]; then
 
         module load SAMtools/1.16.1-GCC-11.3.0
-        samtools sort -@ 6 "$file" -o "bams/$file2"
-        samtools index "bams/$file2"
+        samtools sort -@ 6 "$file" -o "bams2/$file2"
+        samtools index "bams2/$file2"
 
         module load UMI-tools/1.1.2-foss-2022a-Python-3.10.4
-        umi_tools count --per-gene --gene-tag=XT --assigned-status-tag=XS -I "bams/$file2" -S "UMIcounts_g2/${file2}.tsv"
+        umi_tools count --per-gene --gene-tag=XT --assigned-status-tag=XS -I "bams2/$file2" -S "UMIcounts2_g2/${file2}.tsv"
     fi
 done
--- 12_2024_Sl_hs2.sh
+++ 12_2024_Sl_hs2.sh
@@ -3,8 +3,8 @@
 #SBATCH --partition=batch                                                 # Partition (queue) name
 #SBATCH --ntasks=1                                                        # Single task job
 #SBATCH --cpus-per-task=6                                                 # Number of cores per task
-#SBATCH --mem=50gb                                                        # Total memory for job
-#SBATCH --time=6:00:00                                                    # Time limit hrs:min:sec
+#SBATCH --mem=70gb                                                        # Total memory for job
+#SBATCH --time=12:00:00                                                    # Time limit hrs:min:sec
 #SBATCH --output=/scratch/jms53460/12_2024_Sl/Hs2.out                     # Location of standard output file
 #SBATCH --error=/scratch/jms53460/12_2024_Sl/Hs2.err                      # Location of error log file
 #SBATCH --mail-user=jms53460@uga.edu                                      # Where to send mail
@@ -12,29 +12,30 @@
 
 cd /scratch/jms53460/12_2024_Sl
 
-module load fastp/0.23.2-GCC-11.3.0
-mkdir hisat2_out
-for file in Demultiplexed/*s.fastq.gz; do
-	file2="${file:14:-9}"
+#module load fastp/0.23.2-GCC-11.3.0
+mkdir hisat2_out2
+cp hisat2_out/*fastq.gz hisat2_out2
+cp /scratch/jms53460/12_18_2024_Sl/*fastq.gz hisat2_out2
+#for file in Demultiplexed/*s.fastq.gz; do
+#	file2="${file:14:-9}"
 
-if [ ! -f "hisat2_out/""$file2"".bam" ]; then
+#if [ ! -f "hisat2_out/""$file2"".bam" ]; then
 
-	fastp -w 6 -i "$file" -o "hisat2_out/""$file2"".fastq.gz" -y -x -3 -a AAAAAAAAAAAA
+#	fastp -w 6 -i "$file" -o "hisat2_out/""$file2"".fastq.gz" -y -x -3 -a AAAAAAAAAAAA
 
-fi
-done
+#fi
+#done
 
-ml HISAT2/3n-20201216-gompi-2022a
 ml SAMtools/1.16.1-GCC-11.3.0
-for file in hisat2_out/*s.fastq.gz
+for file in hisat2_out2/*s.fastq.gz
 do
-	file2="${file:11:-9}"
+	file2="${file:12:-9}"
 
-if [ ! -f "hisat2_out/""$file2"".bam" ]; then
+if [ ! -f "hisat2_out2/""$file2"".bam" ]; then
 
-	hisat2 -p 6 --dta -x Sl_N-masked_genome_index -U "hisat2_out/""$file2"".fastq.gz" | samtools view -bS -> "hisat2_out/""$file2""_unsorted.bam"
-	samtools sort -@ 6 "hisat2_out/""$file2""_unsorted.bam" -o "hisat2_out/""$file2""_s.bam"
-    samtools index -@ 6 "hisat2_out/""$file2""_s.bam"
+	hisat2 -p 6 --dta -x Sl_N-masked_genome2_index -U "hisat2_out2/""$file2"".fastq.gz" | samtools view -bS -> "hisat2_out2/""$file2""_unsorted.bam"
+	samtools sort -@ 6 "hisat2_out2/""$file2""_unsorted.bam" -o "hisat2_out2/""$file2""_s.bam"
+    samtools index -@ 6 "hisat2_out2/""$file2""_s.bam"
 	
 fi
 done
