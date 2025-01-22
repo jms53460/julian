f3c0c4a2e6fb4fa8eb70da8eebc5b46236383108
jms53460 <jms53460@uga.edu>
2025-01-21 20:12:45 -0500

    rerunning hisat2

--- 11_2024_Sl_hisat2.sh
+++ 11_2024_Sl_hisat2.sh
@@ -12,31 +12,32 @@
 
 cd /scratch/jms53460/11_2024_Sl
 
-module load fastp/0.23.2-GCC-11.3.0
-mkdir hisat2_out
-for file in Demultiplexed/*s.fastq.gz; do
-	file2="${file:14:-9}"
+#module load fastp/0.23.2-GCC-11.3.0
+mkdir hisat2_out2
+cp hisat2_out/*fastq.gz hisat2_out2
+#for file in Demultiplexed/*s.fastq.gz; do
+#	file2="${file:14:-9}"
 
-if [ ! -f "hisat2_out/""$file2"".bam" ]; then
+#if [ ! -f "hisat2_out/""$file2"".bam" ]; then
 
-	fastp -w 6 -i "$file" -o "hisat2_out/""$file2"".fastq.gz" -y -x -3 -a AAAAAAAAAAAA
+#	fastp -w 6 -i "$file" -o "hisat2_out/""$file2"".fastq.gz" -y -x -3 -a AAAAAAAAAAAA
 
-fi
-done
+#fi
+#done
 
 ml HISAT2/3n-20201216-gompi-2022a
-hisat2-build -p 6 Sl_N-masked_genome.fa Sl_N-masked_genome_index
+hisat2-build -p 6 Sl_N-masked_genome2.fa Sl_N-masked_genome2_index
 
 ml SAMtools/1.16.1-GCC-11.3.0
-for file in hisat2_out/*s.fastq.gz
+for file in hisat2_out2/*s.fastq.gz
 do
-	file2="${file:11:-9}"
+	file2="${file:12:-9}"
 
-if [ ! -f "hisat2_out/""$file2"".bam" ]; then
+if [ ! -f "hisat2_out2/""$file2"".bam" ]; then
 
-	hisat2 -p 6 --dta -x Sl_N-masked_genome_index -U "hisat2_out/""$file2"".fastq.gz" | samtools view -bS -> "hisat2_out/""$file2""_unsorted.bam"
-	samtools sort -@ 6 "hisat2_out/""$file2""_unsorted.bam" -o "hisat2_out/""$file2""_s.bam"
-    samtools index -@ 6 "hisat2_out/""$file2""_s.bam"
+	hisat2 -p 6 --dta -x Sl_N-masked_genome2_index -U "hisat2_out2/""$file2"".fastq.gz" | samtools view -bS -> "hisat2_out2/""$file2""_unsorted.bam"
+	samtools sort -@ 6 "hisat2_out2/""$file2""_unsorted.bam" -o "hisat2_out2/""$file2""_s.bam"
+    samtools index -@ 6 "hisat2_out2/""$file2""_s.bam"
 	
 fi
 done
