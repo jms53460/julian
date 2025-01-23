c5a9f4f7ffa0f3e8a0cf994909877b93d3f67c68
jms53460 <jms53460@uga.edu>
2025-01-23 18:02:20 -0500

    Running UMI_tools count without assigned tag specified

--- 11_2024_Sl_features_UMIs.sh
+++ 11_2024_Sl_features_UMIs.sh
@@ -11,59 +11,15 @@
 #SBATCH --mail-type=END,FAIL                                                  # Mail events (BEGIN, END, FAIL, ALL)
 
 cd /scratch/jms53460/11_2024_Sl
-mkdir featurecounts2
-mkdir bams2
-mkdir UMIcounts2
-mkdir UMIcounts2_g1
-mkdir UMIcounts2_g2
-ml purge_dups/1.2.5-foss-2021b
-ml Miniconda3/23.5.2-0
-source activate /home/jms53460/subread-env
-
-featureCounts -T 6 -s 1 -a Sl_14.gff -t 'gene' -g 'ID' -o featurecounts2/read_counts.tab --readExtension5 500 -R BAM SNPsplit2/*_SNPsplit.bam
-featureCounts -T 6 -s 1 -a Sl_14.gff -t 'gene' -g 'ID' -o featurecounts2/read_counts_g1.tab --readExtension5 500 -R BAM SNPsplit2/*_SNPsplit_g1.bam
-featureCounts -T 6 -s 1 -a Sl_14.gff -t 'gene' -g 'ID' -o featurecounts2/read_counts_g2.tab --readExtension5 500 -R BAM SNPsplit2/*_SNPsplit_g2.bam
-
-conda deactivate
+mkdir UMIcounts3
+module load UMI-tools/1.1.2-foss-2022a-Python-3.10.4
 
 for file in "featurecounts2/"*SNPsplit.bam*
 do
     file2="${file:15:-22}"
-    if [ ! -f "UMIcounts2/${file2}.tsv" ]; then
+    if [ ! -f "UMIcounts3/${file2}.tsv" ]; then
 
-        module load SAMtools/1.16.1-GCC-11.3.0
-        samtools sort -@ 6 "$file" -o "bams2/$file2"
-        samtools index "bams2/$file2"
-
-        module load UMI-tools/1.1.2-foss-2022a-Python-3.10.4
-        umi_tools count --per-gene --gene-tag=XT --assigned-status-tag=XS -I "bams2/$file2" -S "UMIcounts2/${file2}.tsv"
-    fi
-done
-
-for file in "featurecounts2/"*g1.bam*
-do
-    file2="${file:15:-22}"
-    if [ ! -f "UMIcounts2_g1/${file2}.tsv" ]; then
-
-        module load SAMtools/1.16.1-GCC-11.3.0
-        samtools sort -@ 6 "$file" -o "bams2/$file2"
-        samtools index "bams2/$file2"
-
-        module load UMI-tools/1.1.2-foss-2022a-Python-3.10.4
-        umi_tools count --per-gene --gene-tag=XT --assigned-status-tag=XS -I "bams2/$file2" -S "UMIcounts2_g1/${file2}.tsv"
-    fi
-done
-
-for file in "featurecounts2/"*g2.bam*
-do
-    file2="${file:15:-22}"
-    if [ ! -f "UMIcounts2_g2/${file2}.tsv" ]; then
-
-        module load SAMtools/1.16.1-GCC-11.3.0
-        samtools sort -@ 6 "$file" -o "bams2/$file2"
-        samtools index "bams2/$file2"
-
-        module load UMI-tools/1.1.2-foss-2022a-Python-3.10.4
-        umi_tools count --per-gene --gene-tag=XT --assigned-status-tag=XS -I "bams2/$file2" -S "UMIcounts2_g2/${file2}.tsv"
+        umi_tools count --per-gene --gene-tag=XT -I "bams2/$file2" -S "UMIcounts3/${file2}.tsv"
+    
     fi
 done
