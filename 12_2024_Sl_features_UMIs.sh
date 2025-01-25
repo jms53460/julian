6967d05a899de6a5230873f58c8563a583314beb
jms53460 <jms53460@uga.edu>
2025-01-25 16:43:28 -0500

    running UMIcounts for S80-91_50s

--- 12_2024_Sl_features_UMIs.sh
+++ 12_2024_Sl_features_UMIs.sh
@@ -3,8 +3,8 @@
 #SBATCH --partition=batch                                                     # Partition (queue) name
 #SBATCH --ntasks=1                                                            # Single task job
 #SBATCH --cpus-per-task=1                                                     # Number of cores per task
-#SBATCH --mem=150gb                                                            # Total memory for job
-#SBATCH --time=24:00:00                                                       # Time limit hrs:min:sec
+#SBATCH --mem=50gb                                                            # Total memory for job
+#SBATCH --time=12:00:00                                                       # Time limit hrs:min:sec
 #SBATCH --output=/scratch/jms53460/12_2024_Sl/Features_UMIs.out            # Location of standard output file
 #SBATCH --error=/scratch/jms53460/12_2024_Sl/Features_UMIs.err             # Location of error log file
 #SBATCH --mail-user=jms53460@uga.edu                                          # Where to send mail
@@ -14,12 +14,4 @@ cd /scratch/jms53460/12_2024_Sl
 mkdir UMIcounts3
 module load UMI-tools/1.1.2-foss-2022a-Python-3.10.4
 
-for file in "featurecounts3/"*SNPsplit.bam*
-do
-    file2="${file:15:-22}"
-    if [ ! -f "UMIcounts3/${file2}.tsv" ]; then
-
-        umi_tools count --per-contig -I "bams2/$file2" -S "UMIcounts3/${file2}.tsv"
-    
-    fi
-done
+umi_tools count --per-contig -I "bams2/S80-91_50s_SNPsplit" -S "UMIcounts3/S80-91_50s_SNPsplit.tsv"
