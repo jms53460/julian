5599948e87c8a40ab78d9b612cc2e46ecfc78365
jms53460 <jms53460@uga.edu>
2025-01-25 22:18:55 -0500

    Added memory

--- 12_2024_Sl_features_UMIs.sh
+++ 12_2024_Sl_features_UMIs.sh
@@ -3,8 +3,8 @@
 #SBATCH --partition=batch                                                     # Partition (queue) name
 #SBATCH --ntasks=1                                                            # Single task job
 #SBATCH --cpus-per-task=1                                                     # Number of cores per task
-#SBATCH --mem=50gb                                                            # Total memory for job
-#SBATCH --time=12:00:00                                                       # Time limit hrs:min:sec
+#SBATCH --mem=250gb                                                            # Total memory for job
+#SBATCH --time=24:00:00                                                       # Time limit hrs:min:sec
 #SBATCH --output=/scratch/jms53460/12_2024_Sl/Features_UMIs.out            # Location of standard output file
 #SBATCH --error=/scratch/jms53460/12_2024_Sl/Features_UMIs.err             # Location of error log file
 #SBATCH --mail-user=jms53460@uga.edu                                          # Where to send mail
