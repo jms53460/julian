e2493029c9becbce5dfb3a6ae697e99080f8ff17
jms53460 <jms53460@uga.edu>
2025-01-27 14:20:43 -0500

    Trying running umitools on S14-25_10s again

--- 12_2024_Sl_features_UMIs.sh
+++ 12_2024_Sl_features_UMIs.sh
@@ -4,14 +4,14 @@
 #SBATCH --ntasks=1                                                            # Single task job
 #SBATCH --cpus-per-task=1                                                     # Number of cores per task
 #SBATCH --mem=250gb                                                            # Total memory for job
-#SBATCH --time=24:00:00                                                       # Time limit hrs:min:sec
+#SBATCH --time=12:00:00                                                       # Time limit hrs:min:sec
 #SBATCH --output=/scratch/jms53460/12_2024_Sl/Features_UMIs.out            # Location of standard output file
 #SBATCH --error=/scratch/jms53460/12_2024_Sl/Features_UMIs.err             # Location of error log file
 #SBATCH --mail-user=jms53460@uga.edu                                          # Where to send mail
 #SBATCH --mail-type=END,FAIL                                                  # Mail events (BEGIN, END, FAIL, ALL)
 
 cd /scratch/jms53460/12_2024_Sl
-mkdir UMIcounts3
+mkdir UMIcounts4
 module load UMI-tools/1.1.2-foss-2022a-Python-3.10.4
 
-umi_tools count --per-contig -I "bams2/S80-91_50s_SNPsplit" -S "UMIcounts3/S80-91_50s_SNPsplit.tsv"
+umi_tools count --per-contig -I "bams2/S14-25_S189_L007_10s_SNPsplit" -S "UMIcounts4/S14-25_S189_L007_10s_SNPsplit.tsv"
