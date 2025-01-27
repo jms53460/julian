da56b529fd3c55c4b426ffc76e491159ff0a15ed
jms53460 <jms53460@uga.edu>
2025-01-27 14:23:48 -0500

    changed time limit

--- 12_2024_Sl_features_UMIs.sh
+++ 12_2024_Sl_features_UMIs.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks=1                                                            # Single task job
 #SBATCH --cpus-per-task=1                                                     # Number of cores per task
 #SBATCH --mem=250gb                                                            # Total memory for job
-#SBATCH --time=12:00:00                                                       # Time limit hrs:min:sec
+#SBATCH --time=6:00:00                                                       # Time limit hrs:min:sec
 #SBATCH --output=/scratch/jms53460/12_2024_Sl/Features_UMIs.out            # Location of standard output file
 #SBATCH --error=/scratch/jms53460/12_2024_Sl/Features_UMIs.err             # Location of error log file
 #SBATCH --mail-user=jms53460@uga.edu                                          # Where to send mail
