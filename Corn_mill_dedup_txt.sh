8b22d4f389fd0ab85f394f35e5c26a46111d0abb
jms53460 <jms53460@uga.edu>
2025-05-07 12:14:34 -0400

    Fixed typo

--- Corn_mill_dedup_txt.sh
+++ Corn_mill_dedup_txt.sh
@@ -10,18 +10,6 @@
 #SBATCH --mail-user=jms53460@uga.edu                                       # Where to send mail
 #SBATCH --mail-type=END,FAIL                                               # Mail events (BEGIN, END, FAIL, ALL)
 
-#!/bin/bash
-#SBATCH --job-name=Corn_mill_Dedup_txt
-#SBATCH --partition=highmem_p
-#SBATCH --ntasks=1                                                         # Single task job
-#SBATCH --cpus-per-task=8                                                  # Number of cores per task
-#SBATCH --mem=100gb                                                        # Total memory for job
-#SBATCH --time=24:00:00                                                    # Time limit hrs:min:sec
-#SBATCH --output=/scratch/jms53460/Corn_mill/Corn_mill_dedup_txt.out       # Location of standard output file
-#SBATCH --error=/scratch/jms53460/Corn_mill/Corn_mill_dedup_txt.err        # Location of error log file
-#SBATCH --mail-user=jms53460@uga.edu                                       # Where to send mail
-#SBATCH --mail-type=END,FAIL                                               # Mail events (BEGIN, END, FAIL, ALL)
-
 cd /scratch/jms53460/Corn_mill
 
 module load UMI-tools/1.1.2-foss-2022a-Python-3.10.4
