b20db3b012a8ff99c2bba79ec7a8c1325c6631c4
jms53460 <jms53460@uga.edu>
2025-07-11 14:04:08 -0400

    Prepared to make tobacco vcf files

--- Ns_DNA_vcf.sh
+++ Ns_DNA_vcf.sh
@@ -3,21 +3,24 @@
 #SBATCH --partition=batch                                          # Partition (queue) name
 #SBATCH --ntasks=1                                                 # Single task job
 #SBATCH --cpus-per-task=6                                          # Number of cores per task
-#SBATCH --mem=50gb                                                 # Total memory for job
-#SBATCH --time=12:00:00                                            # Time limit hrs:min:sec
-#SBATCH --output=/scratch/jms53460/Tobacco_DNAseq/Ns_vcf.out          # Location of standard output file
-#SBATCH --error=/scratch/jms53460/Tobacco_DNAseq/Ns_vcf.err           # Location of error log file
+#SBATCH --mem=200gb                                                # Total memory for job
+#SBATCH --time=24:00:00                                            # Time limit hrs:min:sec
+#SBATCH --output=/scratch/jms53460/Tobacco_DNA_7_2025/Ns_vcf.out   # Location of standard output file
+#SBATCH --error=/scratch/jms53460/Tobacco_DNA_7_2025/Ns_vcf.err    # Location of error log file
 #SBATCH --mail-user=jms53460@uga.edu                               # Where to send mail
 #SBATCH --mail-type=END,FAIL                                       # Mail events (BEGIN, END, FAIL, ALL)
 
-cd /scratch/jms53460/Tobacco_DNAseq
+cd /scratch/jms53460/Tobacco_DNA_7_2025
 module load SAMtools/1.16.1-GCC-11.3.0
-samtools index -@ 6 hisat2_out/TW136_s.bam
-samtools index -@ 6 hisat2_out/TW137_s.bam
-
 module load BCFtools/1.15.1-GCC-11.3.0
-bcftools mpileup -Ou --threads 6 -d 1000000 --min-MQ 60 -f Ns_genome.fna hisat2_out/TW136_s.bam | bcftools call -Ou -m -v --threads 6 | bcftools filter -Oz -e 'QUAL<40 || DP<10' > TW136.vcf.gz
-bcftools index TW136.vcf.gz
+for file in "Raw_Data/"*_R1_001.fastq.gz
+do
+	file2="${file:9:-16}"
+    samtools index -@ 6 hisat2_out/"$file2"_s.bam
 
-bcftools mpileup -Ou --threads 6 -d 1000000 --min-MQ 60 -f Ns_genome.fna hisat2_out/TW137_s.bam | bcftools call -Ou -m -v --threads 6 | bcftools filter -Oz -e 'QUAL<40 || DP<10' > TW137.vcf.gz
-bcftools index TW137.vcf.gz
+    bcftools mpileup -Ou --threads 6 -d 1000000 --min-MQ 60 -f nsyl.fa hisat2_out/"$file2"_s.bam | bcftools call -Ou -m -v --threads 6 | bcftools filter -Oz -e 'QUAL<40 || DP<10' > "$file2".vcf.gz
+    bcftools index "$file2".vcf.gz
+    gzip -d "$file2".vcf.gz
+    bcftools view -i 'TYPE="snp"' "$file2".vcf > "$file2"_snps.vcf
+    bcftools view -i 'GT="het"' "$file2"_snps.vcf > "$file2"_het_snps.vcf
+done
