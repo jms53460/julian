810f3b2083704bdeded30c43bf78a84006f37b1e
jms53460 <jms53460@uga.edu>
2025-09-02 18:40:11 -0400

    Prepared to nmask genome

--- Rice_nmask.sh
+++ Rice_nmask.sh
@@ -4,12 +4,16 @@
 #SBATCH --ntasks=1                                                        # Single task job
 #SBATCH --cpus-per-task=1                                                 # Number of cores per task
 #SBATCH --mem=50gb                                                        # Total memory for job
-#SBATCH --time=12:00:00                                                    # Time limit hrs:min:sec
-#SBATCH --output=/scratch/jms53460/Rice_DNA/Rice_nmask.out                # Location of standard output file
-#SBATCH --error=/scratch/jms53460/Rice_DNA/Rice_nmask.err                 # Location of error log file
+#SBATCH --time=12:00:00                                                   # Time limit hrs:min:sec
+#SBATCH --output=/scratch/jms53460/Rice_DNA2/Rice_nmask.out               # Location of standard output file
+#SBATCH --error=/scratch/jms53460/Rice_DNA2/Rice_nmask.err                # Location of error log file
 #SBATCH --mail-user=jms53460@uga.edu                                      # Where to send mail
 #SBATCH --mail-type=END,FAIL                                              # Mail events (BEGIN, END, FAIL, ALL)
 
-cd /scratch/jms53460/Rice_DNA
-ml BEDTools/2.30.0-GCC-12.2.0
-bedtools maskfasta -fi Nipponbare.fna -fo NRE1-2_N-masked.fa -bed NRE1-2_snps.vcf -fullHeader
+cd /scratch/jms53460/Rice_DNA2
+ml BEDTools/2.31.1-GCC-13.3.0
+for file in "Raw_Data/"*_R1_001.fastq.gz
+do
+	file2="${file:9:-16}"
+bedtools maskfasta -fi Nipponbare_12.fna -fo "$file2"_N-masked_12.fa -bed "$file2"_snps.vcf -fullHeader
+done
