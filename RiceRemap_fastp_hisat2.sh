686cc366cf5d62d6e630af4aa5cd20a91f5abc56
jms53460 <jms53460@uga.edu>
2026-01-15 16:17:03 -0500

    Added _DNA to hisat2_out directory name

--- RiceRemap_fastp_hisat2.sh
+++ RiceRemap_fastp_hisat2.sh
@@ -15,15 +15,15 @@ cd /scratch/jms53460/RiceRemap
 ml HISAT2/2.2.1-gompi-2023a
 hisat2-build Rice_r7.fa Rice_r7_index
 
-mkdir hisat2_out
+mkdir hisat2_out_DNA
 module load fastp/0.23.4-GCC-13.2.0
 module load SAMtools/1.21-GCC-13.3.0
-for file in "Raw_Data/"*_R1_001.fastq.gz
+for file in "Raw_Data_DNA/"*_R1_001.fastq.gz
 do
-	file2="${file:9:-16}"
+	file2="${file:13:-16}"
 
-	fastp -w 6 -i "$file" -I "Raw_Data/""$file2""_R2_001.fastq.gz" -o "hisat2_out/""$file2""_R1_001.fastq.gz" -O "hisat2_out/""$file2""_R2_001.fastq.gz"
+	fastp -w 6 -i "$file" -I "Raw_Data_DNA/""$file2""_R2_001.fastq.gz" -o "hisat2_out_DNA/""$file2""_R1_001.fastq.gz" -O "hisat2_out_DNA/""$file2""_R2_001.fastq.gz"
 
-	hisat2 -p 6 --dta -x Rice_r7_index -1 "hisat2_out/""$file2""_R1_001.fastq.gz" -2 "hisat2_out/""$file2""_R2_001.fastq.gz" | samtools view -bS -> "hisat2_out/""$file2""_unsorted.bam"
-	samtools sort -@ 6 "hisat2_out/""$file2""_unsorted.bam" -o "hisat2_out/""$file2""_s.bam"
+	hisat2 -p 6 --dta -x Rice_r7_index -1 "hisat2_out_DNA/""$file2""_R1_001.fastq.gz" -2 "hisat2_out_DNA/""$file2""_R2_001.fastq.gz" | samtools view -bS -> "hisat2_out_DNA/""$file2""_unsorted.bam"
+	samtools sort -@ 6 "hisat2_out_DNA/""$file2""_unsorted.bam" -o "hisat2_out_DNA/""$file2""_s.bam"
 done
